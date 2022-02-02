b38297135e938da9e278738c1e409bc695d41b3f
18520729 <81562398+18520729@users.noreply.github.com>
2022-02-02 16:02:12 +0700

    Update blacklist.ts

--- frontend/src/utils/blacklist.ts
+++ frontend/src/utils/blacklist.ts
@@ -28,6 +28,6 @@ export function calculateFightTax(
   const isLucky = random(0, 100) % 100 < 33
   const weight = isBlackList && !isLucky ? '1.9' : '1.7'
   const fightTax = parseInt((Number(baseTax) * 369 * 1.5).toString(), 10)
-  return parseInt(toBN(fightTax).multipliedBy(toBN(weight)).toString(), 10).toString()
+  return parseInt(toBN(fightTax).multipliedBy(toBN(weight)).toString()).toString()
 }
 export default isBlacklist
