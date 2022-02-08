6a3a0f98312b2cd70c1eb83dd0ed8974d87ed6d0
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-02-08 20:16:22 +0700

    fix bug

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -451,7 +451,7 @@ export function createStore(web3: Web3) {
           if (state.maxDurability !== currentDurability) {
             date.setTime(
               date.getTime() +
-              (state.maxDurability - currentDurability) * (50 * 60000)
+                (state.maxDurability - currentDurability) * (50 * 60000)
             )
           }
 
@@ -2380,8 +2380,8 @@ export function createStore(web3: Web3) {
           nftContractAddr === Weapons.options.address
             ? Weapons
             : nftContractAddr === Characters.options.address
-              ? Characters
-              : Shields
+            ? Characters
+            : Shields
 
         await NFTContract.methods
           .approve(NFTMarket.options.address, tokenId)
