d9e59471dca3d92640803f37aed7ab4ae446b057
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-02-14 16:05:42 +0700

    add wiki guide for blacklist page

--- frontend/src/views/Blacksmith.vue
+++ frontend/src/views/Blacksmith.vue
@@ -415,6 +415,47 @@
         </div>
       </b-tab>
     </b-tabs>
+
+    <div class="row mt-3">
+      <div class="col-xl-6">
+        <div class="link-container">
+          <div class="text">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-journal-text"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1
+               .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0
+                1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+              />
+              <path
+                d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1
+               1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"
+              />
+              <path
+                d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1
+               0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0
+               1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"
+              />
+            </svg>
+            Docs:
+          </div>
+          <a
+            class="link-to-wiki"
+            href="https://wiki.cryptowar.network/gameplay/what-are-weapons/reforging"
+            target="_blank"
+          >
+            https://wiki.cryptowar.network/gameplay/what-are-weapons/reforging</a
+          >
+        </div>
+      </div>
+    </div>
+
     <b-modal
       class="centered-modal"
       ref="weapon-rename-modal"
@@ -1138,6 +1179,35 @@ export default Vue.extend({
   text-align: center;
 }
 
+.link-container {
+  display: flex;
+  align-items: center;
+  margin: 0 10px;
+  padding: 0 10px;
+  border: 1px solid #3cde9b;
+  font-size: 1.2em;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.link-container .text {
+  display: flex;
+  font-size: 1.2em;
+  padding: 10px 0;
+}
+
+.link-to-wiki {
+  margin-left: 5px;
+  width: 90%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.link-container svg {
+  margin-right: 5px;
+  width: 30px;
+}
+
 @media (max-width: 1000px) {
   .mobile-flip {
     display: flex;
@@ -1195,6 +1265,14 @@ export default Vue.extend({
 
   .burn-weapons.modal-weapons {
     height: 220px;
+  }
+
+  .link-container {
+    font-size: 1em;
+  }
+
+  .link-container svg {
+    width: 25px;
   }
 }
 </style>
