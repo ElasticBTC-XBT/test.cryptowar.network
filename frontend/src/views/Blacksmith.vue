22e2a3bce44346c42be7e3f1fa348810b35262b6
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-02-21 20:33:45 +0700

    repair css

--- frontend/src/views/Blacksmith.vue
+++ frontend/src/views/Blacksmith.vue
@@ -334,7 +334,43 @@
             </div>
           </div>
         </div>
-
+        <div class="col-xl-6">
+          <div class="link-container">
+            <div class="text">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-journal-text"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1
+               .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0
+                1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+                />
+                <path
+                  d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1
+               1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"
+                />
+                <path
+                  d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1
+               0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0
+               1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"
+                />
+              </svg>
+              Docs:
+            </div>
+            <a
+              class="link-to-wiki"
+              href="https://wiki.cryptowar.network/gameplay/what-are-weapons/reforging"
+              target="_blank"
+            >
+              https://wiki.cryptowar.network/gameplay/what-are-weapons/reforging</a
+            >
+          </div>
+        </div>
         <!-- <div class="row mt-3 dust-box" v-if="showReforge && !showReforgeDust">
           <div class="col">
             <div class="d-flex justify-content-space-between">
@@ -415,46 +451,6 @@
         </div>
       </b-tab>
     </b-tabs>
-
-    <div class="row mt-3">
-      <div class="col-xl-6">
-        <div class="link-container">
-          <div class="text">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-journal-text"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1
-               .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0
-                1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
-              />
-              <path
-                d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1
-               1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"
-              />
-              <path
-                d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1
-               0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0
-               1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"
-              />
-            </svg>
-            Docs:
-          </div>
-          <a
-            class="link-to-wiki"
-            href="https://wiki.cryptowar.network/gameplay/what-are-weapons/reforging"
-            target="_blank"
-          >
-            https://wiki.cryptowar.network/gameplay/what-are-weapons/reforging</a
-          >
-        </div>
-      </div>
-    </div>
 
     <b-modal
       class="centered-modal"
@@ -1182,7 +1178,7 @@ export default Vue.extend({
 .link-container {
   display: flex;
   align-items: center;
-  margin: 0 10px;
+  margin: 15px 8px;
   padding: 0 10px;
   border: 1px solid #3cde9b;
   font-size: 1.2em;
