b0408b059629a190e8db1098d946f17932f5f982
Tam Nguyen <nntam2013@gmail.com>
2022-09-19 15:11:22 +0700

    chore: upgrade script

--- migrations/40_upgrade_xBlade_Token.js
+++ migrations/40_upgrade_xBlade_Token.js
@@ -2,8 +2,7 @@ const xBlade = artifacts.require("xBlade");
 const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer) {
-  const proxyAddress = "0x61Fc654aA0185bae02a9A3b110D28C3AE8412CEf";
-  // testnet;
+  const proxyAddress = "0x61Fc654aA0185bae02a9A3b110D28C3AE8412CEf"; // testnet;
   // const proxyAddress = "0x27a339d9B59b21390d7209b78a839868E319301B"; //mainnet
   await upgradeProxy(proxyAddress, xBlade, {
     deployer,
